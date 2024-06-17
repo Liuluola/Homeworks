eca1a14ec1dae29ac2fa310ba3d999a57e64c972
Olga Met <met_ol@mail.ru>
2024-06-17 19:24:38 +0700

    Lesson 2 added

---
+++ 2_Lesson/1_Task/Calculator_class.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:
+
+	double num1;
+	double num2;
+
+	double add()
+	{
+		return num1 + num2;
+	}
+
+	double multiply()
+	{
+		return num1 * num2;
+	}
+
+	double subtract_1_2()
+	{
+		return num1 - num2;
+	}
+
+	double subtract_2_1()
+	{
+		return num2 - num1;
+	}
+
+	double divide_1_2()
+	{
+		return num1 / num2;
+	}
+
+	double divide_2_1()
+	{
+		return num2 / num1;
+	}
+
+	bool set_num1(double num1)
+	{
+		this->num1 = num1;
+
+		if (num1 != 0)
+			return true;
+		else
+			return false;
+	}
+
+	bool set_num2(double num2)
+	{
+		this->num2 = num2;
+
+		if (num2 != 0)
+			return true;
+		else
+			return false;
+	}
+
+};
+
+int main()
+{
+	double num1, num2;
+	setlocale(LC_ALL, "rus");
+
+	Calculator calc;
+
+	for ( ; ; )
+	{
+		
+		for ( ; ; )
+		{
+			std::cout << "Введите num1: ";
+			std::cin >> num1;
+
+			if (calc.set_num1(num1) != true)
+			{
+				std::cout << "Неверный ввод!" << std::endl;
+			}
+
+			else break;
+		}
+
+		for (; ; )
+		{
+			std::cout << "Введите num2: ";
+			std::cin >> num2;
+
+			if (calc.set_num2(num2) != true)
+			{
+				std::cout << "Неверный ввод!" << std::endl;
+			}
+
+			else break;
+		}
+				
+		calc.num1 = num1;
+		calc.num2 = num2;
+
+		std::cout << "num1 + num2 = " << calc.add() << std::endl;
+		std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+		std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+		std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+		std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+		std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl << std::endl;
+
+	}
+
+	return 0;
+}
---
+++ 2_Lesson/2_Task/Counter_class.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <string>
+
+class Counter {
+protected:
+
+	int value;
+
+public:
+
+	Counter()
+	{
+		this->value = 1;
+	}
+
+	Counter(int c_value)
+	{
+		this->value = c_value;
+	}
+
+	int value_up()
+	{
+		this->value = value + 1;
+		return value;
+	}
+
+	int value_dowm()
+	{
+		this->value = value - 1;
+		return value;
+	}
+
+	void getValue()
+	{
+		std::cout << value;
+		std::cout << std::endl;
+	}
+
+	void get(Counter count, std::string command)
+	{
+		for (; ; )
+		{
+			std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+			std::getline(std::cin, command);
+
+
+			if (command == "+")
+			{
+				value = count.value_up();
+			}
+
+			if (command == "-")
+			{
+				value = count.value_dowm();
+			}
+
+			if (command == "=")
+			{
+				count.getValue();
+			}
+
+			if (command == "x")
+			{
+				std::cout << "До свидания!";
+				break;
+			}
+		}
+
+	}
+};
+
+int main()
+{
+	std::string answer, command;
+	int value;
+	setlocale(LC_ALL, "rus");
+
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите Y или N: ";
+	std::getline(std::cin, answer);
+
+
+
+	if (answer == "Y")
+	{
+		std::cout << "Введите начальное значение счётчика: ";
+		std::cin >> value;
+		Counter counts(value);
+		counts.get(counts, command);
+	}
+
+	else
+	{
+		Counter counts;
+		counts.get(counts, command);
+	}
+	return 0;
+}
+
