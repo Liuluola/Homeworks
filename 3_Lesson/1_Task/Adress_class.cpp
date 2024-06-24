4521e5042c332726f5b6d5a46d21ceee5606e6c8
Olga Met <met_ol@mail.ru>
2024-06-24 07:51:29 +0700

    Lesson 3 added

---
+++ 3_Lesson/1_Task/Adress_class.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+class Adress {
+
+private:
+	std::string city;
+	std::string street;
+	std::string house;
+	std::string flat;
+
+public:
+	Adress(std::string &city, std::string &street, std::string &house, std::string &flat)
+		: city(city), street(street), house(house), flat(flat) {};
+
+	Adress() {};
+
+	std::string get_output_address() {
+
+		return city + ", " + street + ", " + house + ", " + flat;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int quant;
+	std::string city, street, house, flat;
+	std::ifstream inFile("C:\\Users\\Heft\\Desktop\\C++\\3_ Основы программирования\\Homework\\3_Lesson\\1_Task\\in.txt");
+	std::ofstream outFile("C:\\Users\\Heft\\Desktop\\C++\\3_ Основы программирования\\Homework\\3_Lesson\\1_Task\\out.txt");
+	
+	if (inFile.is_open()) {
+
+		inFile >> quant;
+	}
+
+	else {
+
+		std::cout << "File can not be open";
+	}
+
+	inFile.ignore();
+
+	Adress* adress = new Adress[quant];
+
+	for (int i = 0; i < quant; ++i) {
+		std::getline(inFile, city);
+		std::getline(inFile, street);
+		std::getline(inFile, house);
+		std::getline(inFile, flat);
+
+		adress[i] = Adress(city, street, house, flat);
+
+		std::cout << city << std::endl;
+		std::cout << street << std::endl;
+		std::cout << house << std::endl;
+		std::cout << flat << std::endl;
+	}
+
+	outFile << quant << std::endl;
+	for (int i = quant - 1; i >= 0; --i) {
+		outFile << adress[i].get_output_address() << std::endl;
+	}
+
+
+	delete[] adress;
+
+	inFile.close();
+	outFile.close();
+
+	return 0;
+}
---
+++ 3_Lesson/2_Task/Sort_Address.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+struct Address {
+	std::string city;
+	std::string street;
+	int house;
+	int apartment;
+};
+
+void bubbleSort(Address arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j].city > arr[j + 1].city) {
+
+				Address temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+
+int main() {
+	int N;
+	std::ifstream inFile("C:\\Users\\Heft\\Desktop\\C++\\3_ Основы программирования\\Homework\\3_Lesson\\2_Task\\in.txt");
+	std::ofstream outFile("C:\\Users\\Heft\\Desktop\\C++\\3_ Основы программирования\\Homework\\3_Lesson\\2_Task\\out.txt");
+
+	if (inFile.is_open()) {
+
+		inFile >> N;
+		inFile.ignore();
+	}
+
+	else {
+
+		std::cout << "File can not be open";
+	}
+
+	Address* addresses = new Address[N];
+
+	for (int i = 0; i < N; ++i) {
+		std::getline(inFile, addresses[i].city);
+		std::getline(inFile, addresses[i].street);
+		inFile >> addresses[i].house >> addresses[i].apartment;
+		inFile.ignore();
+	}
+
+	bubbleSort(addresses, N);
+
+	outFile << N << std::endl;
+
+	for (int i = 0; i < N; ++i) {
+		outFile << addresses[i].city << ", " << addresses[i].street << ", "
+			<< addresses[i].house << ", " << addresses[i].apartment << std::endl;
+	}
+
+	delete[] addresses;
+
+	inFile.close();
+	outFile.close();
+
+	return 0;
+}
